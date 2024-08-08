0eb5410ea096c58853b3b7a8578fb6e7e4977b57
Hung Pham <hungphammanh2012003@gmail.com>
2024-08-08 19:09:08 +0700

    customize axios

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { LuImagePlus } from "react-icons/lu";
-import axios from 'axios';
+import { toast } from 'react-toastify';
+import { postCreateNewUser } from '../../../services/apiServices';
 
 const ModalCreateUser = (props) => {
     const { show, setShow } = props;
@@ -30,24 +31,33 @@ const ModalCreateUser = (props) => {
             // setPreviewImage("")
         }
     }
+    const validateEmail = (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
 
     const handleSubmitCreateUser = async () => {
-        // let data = {
-        //     email: email,
-        //     password: password,
-        //     username: username,
-        //     role: role,
-        //     userImage: image,
+        //validate
+        // const isValidEmail = validateEmail(email);
+        // if (!isValidEmail) {
+        //     toast.error('Invalid email!')
+        //     return;
         // }
-        const data = new FormData();
-        data.append('email', email);
-        data.append('password', password);
-        data.append('username', username);
-        data.append('role', role);
-        data.append('userImage', image);
-
-        let res = await axios.post('http://localhost:8081/api/v1/participant', data)
-        console.log(">>> check res: ", res)
+        if (!password) {
+            toast.error('Invalid password!')
+            return;
+        }
+        let res = await postCreateNewUser(email, password, username, role, image);
+        if (res && res.EC === 0) {
+            toast.success(res.EM);
+            handleClose();
+        }
+        if (res && res.EC !== 0) {
+            toast.error(res.EM);
+        }
     }
     return (
         <>
