e64bbe2fc4bbf2a81d0462b94d4d1f224ad711f7
Hung Pham <hungphammanh2012003@gmail.com>
2024-08-11 15:12:54 +0700

    complete-get-users

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -2,6 +2,7 @@ import ModalCreateUser from "./ModalCreateUser";
 import './ManageUser.scss'
 import { FcPlus } from "react-icons/fc";
 import { useState } from "react";
+import TableUser from "./TableUser";
 
 const ManageUser = (props) => {
     const [showModalCreateUser, setShowModalCreateUser] = useState(false);
@@ -15,12 +16,12 @@ const ManageUser = (props) => {
                     <button className="btn btn-primary" onClick={() => setShowModalCreateUser(true)}><FcPlus />Add new user</button>
                 </div>
                 <div className="table-users-container">
-                    table users
-                    <ModalCreateUser
-                        show={showModalCreateUser}
-                        setShow={setShowModalCreateUser}
-                    />
+                    <TableUser />
                 </div>
+                <ModalCreateUser
+                    show={showModalCreateUser}
+                    setShow={setShowModalCreateUser}
+                />
             </div>
         </div>
     )
