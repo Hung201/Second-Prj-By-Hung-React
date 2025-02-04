3d630debcb448209c584443448c14905ad9b418a
Hung Pham <hungphammanh2012003@gmail.com>
2025-02-04 23:35:47 +0700

    106 Private route

--- src/Layout.js
+++ src/Layout.js
@@ -14,6 +14,7 @@ import DetailQuiz from './components/User/DetailQuiz';
 import ManageQuiz from './components/Admin/Content/Quiz/ManageQuiz';
 import Question from './components/User/Question';
 import Questions from './components/Admin/Content/Question/Questions';
+import PrivateRoute from './routes/PrivateRoute';
 
 const NotFound = () => {
     return (
@@ -28,10 +29,20 @@ const Layout = (props) => {
             <Routes>
                 <Route path="/" element={<App />} >\
                     <Route index element={<HomePage />} />
-                    <Route path="users" element={<ListQuiz />} />
+                    <Route path="users" element={
+                        <PrivateRoute>
+                            <ListQuiz />
+                        </PrivateRoute>
+                    }
+                    />
                 </Route>
                 <Route path="/quiz/:id" element={<DetailQuiz />} />
-                <Route path="/admin" element={<Admin />} >
+                <Route path="/admin" element={
+                    <PrivateRoute>
+                        <Admin />
+                    </PrivateRoute>
+                }
+                >
                     <Route index element={<DashBoard />} />
                     <Route path="manage-users" element={<ManageUser />} />
                     <Route path="manage-quizzes" element={<ManageQuiz />} />
